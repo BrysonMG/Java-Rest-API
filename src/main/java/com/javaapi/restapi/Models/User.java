aaa25a46e3ec493a1f396713d27e6e3ae3daff5c
Bryson Goins <brysonmg@gmail.com>
2021-09-03 14:57:35 -0500

    added User model with getters/setters

---
+++ src/main/java/com/javaapi/restapi/Models/User.java
@@ -0,0 +1,51 @@
+package com.javaapi.restapi.Models;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String occupation;
+
+    //Getters and Setters have to be declared separately. You can't do { get; set; } like you could in C#.
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+}
