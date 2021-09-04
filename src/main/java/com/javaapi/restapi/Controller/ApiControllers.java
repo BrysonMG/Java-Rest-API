2d659a0019e48d6da945b168d2e09fe89cb620ed
Bryson Goins <brysonmg@gmail.com>
2021-09-04 01:56:56 -0500

    in progress

--- src/main/java/com/javaapi/restapi/Controller/ApiControllers.java
+++ src/main/java/com/javaapi/restapi/Controller/ApiControllers.java
@@ -1,13 +1,37 @@
 package com.javaapi.restapi.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.javaapi.restapi.Models.User;
+import com.javaapi.restapi.Repo.IUserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ApiControllers {
 
+    @Autowired
+    private IUserRepo userRepo;
+
     @GetMapping(value = "/")
     public String getPage() {
+
         return "Welcome";
+    }
+
+    @GetMapping(value="/users")
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+
+    @PostMapping(value="/add")
+    public String addUser(@RequestBody User user) {
+        userRepo.save(user);
+        return "Saved!";
+    }
+
+    @PutMapping("update/{id}")
+    public String updateUser(int id, @RequestBody User user) {
+        User updatedUser = userRepo.findById(id).get();
     }
 }
